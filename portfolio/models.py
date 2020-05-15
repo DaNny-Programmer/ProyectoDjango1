a612c8239ed5415f4a9bc05ed5361f634be3ab72
Daniela Muñoz <daniimhdez08@gmail.com>
2020-05-14 20:28:13 -0500

    Commit- Personalización del Panel Administrador

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated =models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200, verbose_name="Título")
+    description = models.TextField(verbose_name="Descripción")
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de Edición")
+
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural= "proyectos"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
