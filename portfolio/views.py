c9d1122e788dabd22540c6bc10960e5ff340f82d
Daniela Muñoz <daniimhdez08@gmail.com>
2020-05-14 21:33:12 -0500

    Commit- MVT Model

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Project
-
-# Create your views here.
+#create your views here.
 def portfolio(request):
-    projects = Project.objects.all()
-    return render(request, "portfolio/portfolio.html", {'proyects': projects})
+    projects= Project.objects.all()
+    return render(request, "portfolio/portfolio.html", {'projects':projects})
